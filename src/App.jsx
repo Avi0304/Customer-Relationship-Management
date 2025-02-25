3698431926804a7abaadb863954fe74431475e3d
AAYUSH-1210 <ap3017015@gmail.com>
2025-02-25 10:51:04 +0530

    Added Task Management component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DashBoard from "./pages/DashBoard";
+import TaskManagement from "./components/TaskManagement";
 
 function App() {
   return (
+    <>
     <Router>
       <Routes>
         <Route path="/" element={<DashBoard />} />
+        <Route path="/path" element={<TaskManagement/>} />
       </Routes>
     </Router>
+    </>
   );
 }
 
