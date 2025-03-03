6e3d415a035d6693f74c18df45d96e4203a370c4
Avi0304 <avip56325@gmail.com>
2025-03-03 14:30:37 +0530

    Added Lead Management Page

---
+++ src/pages/LeadPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import TopNav from "../components/TopNav";
+import Sidebar from "../components/SideBar";
+import LeadManagement from "../components/LeadManagement";
+
+const LeadPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <div className="flex">
+        <Sidebar />
+
+        <div className="flex-1">
+          <TopNav title={"Task Management"} />
+
+          <main className="p-6 space-y-4">
+            <LeadManagement />
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeadPage;
